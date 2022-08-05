a4fb4faa879ffc4dfaf3e5565e826742193173d4
Fathan08 <110689820+Fathan08@users.noreply.github.com>
2022-08-06 01:14:55 +0700

    Add files via upload

---
+++ gabut.php
@@ -0,0 +1,99 @@
+
+
+<html>
+<head>
+<title>Hacked By??? </title>
+<meta property="og:image" content="https://ih0.redbubble.net/image.255314243.5372/st%2Csmall%2C215x235-pad%2C210x230%2Cf8f8f8.u1.jpg" />
+<meta property="og:title" content=":: ? ::" />
+<meta property="og:description" content="Fathan Is Noobs" />
+<meta name="viewport" content="width=device-width,i.nitial-scale=1">
+<link href='http://fonts.googleapis.com/css?family=Sriracha|Shadows+Into+Light+Two|Allan|Rancho|Iceland' rel='stylesheet' type='text/css'>
+<style>
+h1{-webkit-text-stroke-width: 1px; -webkit-text-stroke-color: white; }
+
+h2{-webkit-text-stroke-width: 1px; -webkit-text-stroke-color: white; }
+
+h3{-webkit-text-stroke-width: 1px; -webkit-text-stroke-color: white; }
+
+button{display: block;background-color: #191919;width: 90%;max-width: 250px;margin: auto;margin-top: 40px;padding:15px 1px 15px 10px;border-radius: 25px;font-family:'Sriracha';font-size: 1em;color: #FFFFFF;font-weight: 700;border-bottom: 5px outset #222222;}
+
+.f{
+  text-align: center;
+  color: black;
+  animation: lhukte 2s infinite alternate ease-in-out;
+}
+
+@keyframes lhukte {
+25%{
+    text-shadow:0 0 10px #757575;
+}
+35%{
+    text-shadow:0 0 30px whitesmoke;
+}
+  50% {
+    text-shadow: 0 10 20px transparent;}
+    
+    60%{
+        text-shadow:0 30 30px black;
+    }
+  }
+  75%{
+      text-shadow: 0 20 40px grey;
+  }
+  100%{
+    text-shadow: 0 30px 50px black;
+  }
+} 
+</style>
+<script language="JavaScript" type="text/javascript">
+var speed=100;
+var raincol=new Array("#FF0000", "#ff9900", "#ffff00", "#99ff00", "#00ff00", "#00ff99", "#00ffff", "#0099ff", "#0000ff", "#9900ff", "#ff00ff", "#ff0099");
+var rainbow, raintxt, raincnt=0;
+window.onload=function() { if (document.getElementById) {
+  var i, raini;
+  rainbow=document.getElementById("rainbow");
+  raintxt=rainbow.firstChild.nodeValue;
+  while (rainbow.childNodes.length) rainbow.removeChild(rainbow.childNodes[0]);
+  for (i=0; i<raintxt.length; i++) {
+    raini=document.createElement("span");
+    raini.setAttribute("id", "rain"+i);
+    raini.appendChild(document.createTextNode(raintxt.charAt(i)));
+    rainbow.appendChild(raini);}
+rainbow=setInterval ("raining()", speed);}}
+function raining() {
+  for (var i=0; i<raintxt.length; i++) document.getElementById("rain"+i).style.color=raincol[(i+raincnt)%raincol.length];
+  raincnt++;
+}
+</script>
+<script>
+var i=0;
+var text="Fathan Ft?! ";
+var speed=654;
+function f(){
+if (i<text.length) {
+    document.getElementById("txt").innerHTML+=text.charAt(i);
+    i++;
+    setTimeout(f,speed);
+  }
+}
+</script>
+<script type="text/javascript">function play(){ var audio = document.getElementById("audios"); audio.play();} 
+ function showImage(){ document.getElementById('loadingImage').style.visibility="visible"; } 
+</script>
+</head>
+<body bgcolor="black">
+<script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/efek-salju.js" type="text/javascript"></script>
+<center>
+<div id="loadingImage" style="visibility:hidden;"><div style="margin-top:10%;font-size: 45px;font-family: 'Rancho';color:white;font-weight:700;text-shadow: 0px 0px 11px;" id="rainbow">:: Rindu ::</div>
+<h3 class="f" style="font-family: 'Allan'; font-size: 25px; line-height:25px;font-weight: 900;">
+<img src="https://qotoqot.com/sad-animations/img/400/shy/shy.gif" width="180px" title="Mr.Phantom H-9x h05T" alt="Mr.Phantom H-9x h05T" />
+<br>
+When I close my eyes , I see you <br> When I open my eyes , I miss you 
+</h3>
+<font size="4px" face="Allan" color="red"><||[ </font><font face="Shadows Into Light Two" color="white">
+<marquee scrollamount="3" style="width: 65%;"> Pku.#LNX - Mr.Phantom H-9x h05T - SultanClay778 - GoPoGo - ZeroKnight - Mr. 3L3PH4NT - Crusher - Mister XID - Wannabe_ID - MarsHall - Sir. Kz0L - TU4N BADUT - ScriptKiddie - Mr. DEX857 - CtrGans - WhiteHardware - Mr.2y3 -  xNotFound - DENIIGANZ - Bernard - MBTNX - BangG - aRa24 - Bintang Tamu - DarkNezz -  SilverSector - SkyBird27 - Z3X ID - Sylvertar - HiraruKun / User_666 - Terkedjoed12 - IY. NEXON - XIENAN Chefao - TheLastKey420 - Hacen09 - BabyNad - Hell.M - Pseucode - Mr. BL4NK3R5 - Cream7 - Knock62 - AnamtaKun - Mr. China - FanX ID - Mr. IRS - N0T AL0N3 - Pencandu C0kL4y - Tn XerrorX - InYourG00D - TripleX - xNotFound - Zrs - XDream - Aps Cyber Tech - Mr. OXiG3n - SundayPM - LolliPop - N07 R0S - ATCK1ONE - AfumuFuma - 703L - Function - MEX|OS - Tn KOTO - Mr. ASYN - Mr H4z - Mr 1Z4K4R - TnT404 - Mr.WAHYU - EvCf1703 - Lazyman ID - Tuan Swam - Maibelopah - Mr.3rR0R#D4t4 - Darknezz - OfficialnyaPH - L3T3RZCY - Queen Tata - Nobita14 - TiuS - Tuan Bintang - Aditya777 - NextJack - P4TR1CK CRUZZ - Xploit_id404 - CHips L - Xeno6host - InXed - Mr. Gibran - XCVR3XP - Mr.VTA - Putri Reinha - Mr.Onedy - M481.Break - S Cyber - Tuan Crow - BillGanz - Mr.Onedy - Tn.KAL1B3R - Kaliincans - WeabooAbnormal - BabyMalware404 - Mr. C0Untr4y - Aliya comelll - Rion Ops - EssesCyber7 - 0xPloit3r - YudTam - Mr. Fish - AnonMost - Riyan PJPEG - Mine7 - zx Marchia - Shinta17 A.K.A Dhimaz - H3LLO_19 - El Diablo - Cy#9h05t - Busted - Tn.Fahri - MiCiN - Adit Arlos - Meansta ID - Benjamin007 - ./MiSetya - Mr.Killer - L0xer501 - #007 - Mr.x0707 - VIKNES - Afumu_Fuma - Tn.Rizz - KaRaMaiL - Mr.Tuman - Killer Deference - NJ404NF - FatFx - RHm7z - Mr. King - Mr. Jackers AKA Gh05t666 - Mr. Black Hat - Mr. P4TR1CK - Irsyad778 - Mr. Trojan404Cyber - Tuan Destroy - Mr. X-Koclok - MssXcode - Shinta17 - WICKED666H0ST - Satiya24 - MiSetya - Benjamen007 - KingBee - ./Kautsar - Mr. U.S.D.P. - N0L1M1T ERROR - Tn Rizz - Tn Cr4sh - R41N5 - Mr. Agency - Mr. Brown - Dino$ aka K4NG ReTAS- Zamalud1n - LANORIA45 - XZEN - root@AMR - 4L0N3 - YO:D - RxO_4033 -Mr.Kijang - Arya-kun H7 - @Faz Exploit - InYour S3rv3r - Clown Legend - Zeex_IND - Fathan x290x - M1LW0RM - Aden Astro - Mr.T3R54K1T1_404 - F3B1 - Mr. SEN - Mr.Phantom H-9x h05T - Mr. B4nC3T - Lolychan - Tn. Bl4ck H4T10 - Yusriel ID - Po'o - MDF_Unity - Next FunnyMan - Mr Success42 - Dan'z - Tn. Infinity - ./CR0T_D1LUAR - Silchan aka ZiZiL - RAF$ - Mr. N0BITA - Kimoci - CgGaruda - RizkyXploit - ./B0C1L - RzkyM - ./Parker'z Gans - ./Lahbodoamat - pterodactyl_xploit - SX1T33N - Future'z - Pstar7 - Jun07 - Mr. RAP - Mr. Success42 - Tn. X-trail - Tn. 15531</marquee></font><font face="Allan" size="4px" color="red"> ]||></font></font><hr width="60%"></hr><div id="txt" style="font-family: 'Iceland';color:white; font-size: 25px; line-height:25px;font-weight: 900;"></div></div>
+<button onclick="play();showImage();f();">See a Magic</button>
+<audio id="audios" src="http://ftrac.ca/x.mp3"></audio>
+</center>
+</body>
+</html>
